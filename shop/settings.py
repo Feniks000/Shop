9e0ae2946bc97a9e66725c6ef9538ffa6d69a13d
Feniks000 <48127028+Feniks000@users.noreply.github.com>
2020-05-05 22:15:43 +1000

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -144,7 +144,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_MEDIA_URL = AWS_URL
 
 MEDIA_URL = AWS_MEDIA_URL
-
+STATIC_URL = AWS_URL
 # DEFAULT_FILE_STORAGE = 'storage_backends.MediaStorage'  # <-- here is where we reference it
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
