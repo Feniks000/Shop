11f70ab8ce32ed95ea0105e8aaea89704dd0ddf1
Feniks000 <48127028+Feniks000@users.noreply.github.com>
2020-05-07 23:09:13 +1000

    email final try2

--- shop/settings.py
+++ shop/settings.py
@@ -153,8 +153,10 @@ if DEBUG:
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+MAILER_EMAIL_BACKEND = EMAIL_BACKEND
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'noreply.starshop25@gmail.com'
 EMAIL_HOST_PASSWORD = 'wsAu4s6Fs54QfSM'
 EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
