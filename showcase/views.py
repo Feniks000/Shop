f1da3e351500353903daddbadc6730eadc4414f6
Feniks000 <48127028+Feniks000@users.noreply.github.com>
2020-05-07 17:58:39 +1000

    Update views.py

--- showcase/views.py
+++ showcase/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
 from .models import Star
 
 
