e8baaadf65b8d665fb49766f007e7e7d3cd1a53b
Feniks000 <48127028+Feniks000@users.noreply.github.com>
2020-05-07 23:02:40 +1000

    email final try1

--- shop/settings.py
+++ shop/settings.py
@@ -157,4 +157,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'noreply.starshop25@gmail.com'
 EMAIL_HOST_PASSWORD = 'wsAu4s6Fs54QfSM'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
