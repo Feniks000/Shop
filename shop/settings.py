49669e9299c855325ac92843039b229a0b6aebb0
Feniks000 <48127028+Feniks000@users.noreply.github.com>
2020-05-09 00:35:05 +1000

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -17,8 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_DJANGO_KEY')
 
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'starshop25.herokuapp.com',
